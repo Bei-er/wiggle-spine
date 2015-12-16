aa3e876909ac12179d83ee1709d0973f6cd4f73f
Micah Elizabeth Scott <micah@misc.name>
2015-12-15 21:25:59 -0800

    Generate some DMXish test signals

--- fpga/testbed.v
+++ fpga/testbed.v
@@ -17,7 +17,8 @@ module top(
 	output LED2,
 	output LED3,
 	output LED4,
-	output LED5
+	output LED5,
+	output DEBUG1
 	);
 
 	reg [21:0] divider;
@@ -28,7 +29,7 @@ module top(
 	assign RS232_TX = RS232_RX;
 
 	assign LED1 = RS232_RX;
-	assign LED2 = 0;
+	assign LED2 = dmx_data;
 	assign LED3 = 0;
 	assign LED4 = 0;
 	assign LED5 = blink;
@@ -43,7 +44,7 @@ module top(
 
 	wire dmx_data;
 	dmx_modulator _dmx_modulator (CLK12, dmx_data, DMX_GATE1, DMX_GATE2, DMX_TX1, DMX_TX2);
-	dmx_packetizer _dmx_packetizer (CLK12, dmx_data);
+	dmx_packetizer _dmx_packetizer (CLK12, dmx_data, DEBUG1);
 endmodule
 
 module dmx_modulator(
@@ -54,7 +55,6 @@ module dmx_modulator(
 	output DMX_TX1,
 	output DMX_TX2
 	);
-
 	reg [6:0] divider;
 	always @(posedge CLK12)
 		divider <= divider + 1;
@@ -65,24 +65,78 @@ module dmx_modulator(
 	assign DMX_GATE2 = !(power_modulation && !power_alt);
 
 	wire data_modulation = divider[0];
-	assign DMX_TX1 = !(data && data_modulation);
-	assign DMX_TX2 = !(data && !data_modulation);
+	assign DMX_TX1 = !(!data && data_modulation);
+	assign DMX_TX2 = !(!data && !data_modulation);
 endmodule
 
 module dmx_packetizer(
 	input CLK12,
-	output dmx_data
+	output dmx_data,
+	output DEBUG1
 	);
-
-	reg [7:0] baudgen;
+	reg [5:0] baudgen;
+	reg [4:0] bit_count;
+	reg [8:0] slot_count;
+	reg [7:0] dmx_value;
+	reg start_state;
 	reg dmx_data;
+
+	reg [7:0] dmx_debug_counter;
+
+	assign DEBUG1 = start_state;  // oscope sync
 
 	always @(posedge CLK12)
 		if (baudgen)
 			baudgen <= baudgen - 1;
 		else begin
-			dmx_data <= !dmx_data;
 			baudgen <= 12_000_000 / 250_000;
+
+			if ((start_state && bit_count == 27) ||
+				(!start_state && bit_count == 10))
+			begin
+				// Start bit, either of the start state or a slot
+				bit_count <= 0;
+				dmx_data <= 0;
+
+				if (start_state) begin
+					start_state <= 0;
+					slot_count <= 0;
+				end
+				else begin
+					slot_count <= slot_count + 1;
+					if (slot_count == 511) begin
+						// Starting a new packet
+						dmx_debug_counter <= dmx_debug_counter + 1;
+
+						// Packet start code 0x00 in the first slot
+						dmx_value <= 0;
+						start_state <= 1;
+					end
+					else begin
+						// Other slots get 'real' data
+						dmx_value <= slot_count == 0 ? 128 :					// dimmer function select
+						             slot_count == 1 ? dmx_debug_counter :		// red
+						             slot_count == 2 ? dmx_debug_counter :		// green
+						             slot_count == 3 ? dmx_debug_counter :		// blue
+						             slot_count == 4 ? dmx_debug_counter :		// white
+						             0;
+					end
+				end
+			end
+			else begin
+				bit_count <= bit_count + 1;
+
+				if (start_state) begin
+				    // During the start state, count bits instead of using the shift register
+				    dmx_data <= bit_count > 23;
+				end
+			    else begin
+			    	// Shift out data bytes
+					dmx_data <= dmx_value[0];
+					dmx_value <= { 1'b1, dmx_value[7:1] };
+				end
+			end
 		end
+
 endmodule
 
