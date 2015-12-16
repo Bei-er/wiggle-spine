179d4ee4f91e2b637bf63482794857487846c738
Micah Elizabeth Scott <micah@misc.name>
2015-12-16 14:12:04 -0800

    Green and purple "hello world" for the DMX port

--- fpga/testbed.v
+++ fpga/testbed.v
@@ -18,7 +18,8 @@ module top(
 	output LED3,
 	output LED4,
 	output LED5,
-	output DEBUG1
+
+	output [7:0] DEBUG
 	);
 
 	reg [21:0] divider;
@@ -29,7 +30,7 @@ module top(
 	assign RS232_TX = RS232_RX;
 
 	assign LED1 = RS232_RX;
-	assign LED2 = dmx_data;
+	assign LED2 = dmx_serial;
 	assign LED3 = 0;
 	assign LED4 = 0;
 	assign LED5 = blink;
@@ -42,9 +43,20 @@ module top(
 	assign GREEN_N = !(divider[7:0] < green_pwm);
 	assign BLUE_N = !(divider[7:0] < blue_pwm);
 
-	wire dmx_data;
-	dmx_modulator _dmx_modulator (CLK12, dmx_data, DMX_GATE1, DMX_GATE2, DMX_TX1, DMX_TX2);
-	dmx_packetizer _dmx_packetizer (CLK12, dmx_data, DEBUG1);
+	wire dmx_serial;
+	wire [8:0] dmx_slot;
+	wire [7:0] dmx_byte;
+	dmx_modulator _dmx_modulator (CLK12, dmx_serial, DMX_GATE1, DMX_GATE2, DMX_TX1, DMX_TX2);
+	dmx_packetizer _dmx_packetizer (CLK12, dmx_serial, dmx_slot, dmx_byte, DEBUG);
+
+	assign dmx_byte =
+		dmx_slot == 0 ? 255 :
+		dmx_slot == 1 ? (blink ? 255 : 5) :
+		dmx_slot == 2 ? (blink ? 5 : 255) :
+		dmx_slot == 3 ? (blink ? 255 : 5) :
+		0;
+
+
 endmodule
 
 module dmx_modulator(
@@ -72,71 +84,90 @@ endmodule
 module dmx_packetizer(
 	input CLK12,
 	output dmx_data,
-	output DEBUG1
+	output [8:0] slot_count,
+	input [7:0] slot_byte,
+	output [7:0] DEBUG
 	);
+
+	parameter S_IDLE = 0;
+	parameter S_BREAK = 1;
+	parameter S_MAB = 2;
+	parameter S_SLOTS = 3;
+
+	parameter BREAK_BITS = 25;
+	parameter MAB_BITS = 3;
+	parameter SLOT_BITS = 11;
+	parameter IDLE_BITS = 20;
+	parameter SLOT_COUNT = 30;
+
 	reg [5:0] baudgen;
+	reg [1:0] state;
 	reg [4:0] bit_count;
 	reg [8:0] slot_count;
-	reg [7:0] dmx_value;
-	reg start_state;
+	reg [8:0] shifter;  // Includes start bit; stop bits are implied
 	reg dmx_data;
 
-	reg [7:0] dmx_debug_counter;
+assign DEBUG = { bit_count, state, dmx_data };
 
-	assign DEBUG1 = start_state;  // oscope sync
-
-	always @(posedge CLK12)
+	always @(posedge CLK12) begin
 		if (baudgen)
 			baudgen <= baudgen - 1;
 		else begin
-			baudgen <= 12_000_000 / 250_000;
-
-			if ((start_state && bit_count == 27) ||
-				(!start_state && bit_count == 10))
-			begin
-				// Start bit, either of the start state or a slot
-				bit_count <= 0;
-				dmx_data <= 0;
-
-				if (start_state) begin
-					start_state <= 0;
-					slot_count <= 0;
-				end
-				else begin
-					slot_count <= slot_count + 1;
-					if (slot_count == 511) begin
-						// Starting a new packet
-						dmx_debug_counter <= dmx_debug_counter + 1;
-
-						// Packet start code 0x00 in the first slot
-						dmx_value <= 0;
-						start_state <= 1;
+			baudgen <= 12_000_000 / 250_000 - 1;
+			case (state)
+				S_IDLE: begin
+					dmx_data <= 1;
+					if (bit_count != 0) begin
+						bit_count <= bit_count - 1;
 					end
 					else begin
-						// Other slots get 'real' data
-						dmx_value <= slot_count == 0 ? 128 :					// dimmer function select
-						             slot_count == 1 ? dmx_debug_counter :		// red
-						             slot_count == 2 ? dmx_debug_counter :		// green
-						             slot_count == 3 ? dmx_debug_counter :		// blue
-						             slot_count == 4 ? dmx_debug_counter :		// white
-						             0;
+						state <= S_BREAK;
+						bit_count <= BREAK_BITS - 1;
 					end
 				end
-			end
-			else begin
-				bit_count <= bit_count + 1;
-
-				if (start_state) begin
-				    // During the start state, count bits instead of using the shift register
-				    dmx_data <= bit_count > 23;
+				S_BREAK: begin
+					dmx_data <= 0;
+					if (bit_count != 0) begin
+						bit_count <= bit_count - 1;
+					end
+					else begin
+						state <= S_MAB;
+						bit_count <= MAB_BITS - 1;
+					end
 				end
-			    else begin
-			    	// Shift out data bytes
-					dmx_data <= dmx_value[0];
-					dmx_value <= { 1'b1, dmx_value[7:1] };
+				S_MAB: begin
+					dmx_data <= 1;
+					if (bit_count != 0) begin
+						bit_count <= bit_count - 1;
+					end
+					else begin
+						// First slot is the start code 0x00
+						state <= S_SLOTS;
+						shifter <= 9'h00;
+						bit_count <= SLOT_BITS - 1;
+						slot_count <= 0;
+					end
 				end
-			end
+				S_SLOTS: begin
+					dmx_data <= shifter[0];
+					if (bit_count != 0) begin
+						bit_count <= bit_count - 1;
+						shifter <= { 1'b1, shifter[8:1] };
+					end
+					else begin
+						if (slot_count == SLOT_COUNT) begin
+							state <= S_IDLE;
+							bit_count <= IDLE_BITS - 1;
+						end
+						else begin
+							bit_count <= SLOT_BITS - 1;
+							slot_count <= slot_count + 1;
+						end
+						shifter <= { slot_byte, 1'b0 };
+					end
+				end
+			endcase
 		end
-
+	end
 endmodule
 
